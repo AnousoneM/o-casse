b8973927346c340ad5bcc75f359d374228d29abe
AnousoneM <anousone.mounivongs@gmail.com>
2025-02-27 16:51:44 +0100

    mise à jour mineure pour les commandes

--- commandes.php
+++ commandes.php
@@ -3,35 +3,35 @@
 <div class="container bg-light rounded pt-5 pb-3 my-5">
     <h1 class="text-center">Commande #1</h1>
 
-    <table class="table table sm">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">nom</th>
-      <th scope="col">prix unitaire</th>
-      <th scope="col">quantité</th>
-      <th scope="col">sous-total</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-    </tr>
+    <table class="table table-sm table-bordered">
+        <thead class="text-center">
+            <tr class="d-flex">
+                <th scope="col" class="col-2">#</th>
+                <th scope="col" class="col-4">nom</th>
+                <th scope="col" class="col-2">prix unitaire</th>
+                <th scope="col" class="col-2">quantité</th>
+                <th scope="col" class="col-2">sous-total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="d-flex">
+                <th scope="row" class="col-2">1</th>
+                <td class="col-4">Mark</td>
+                <td class="col-2">Otto</td>
+                <td class="col-2">@mdo</td>
+                <td class="col-2">@mdo</td>
+            </tr>
+            <tr class="d-flex">
+                <th scope="row" class="col-2">2</th>
+                <td class="col-4">Jacob</td>
+                <td class="col-2">Thornton</td>
+                <td class="col-2">@fat</td>
+                <td class="col-2">@fat</td>
+            </tr>
 
-  </tbody>
-</table>
- 
+        </tbody>
+    </table>
+
     <a class="btn btn-secondary text-center col-4 d-block mx-auto m-2" href="index.php">Retour</a>
 </div>
 
