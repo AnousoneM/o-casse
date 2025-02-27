d2d3d8b3f9105b77d62b123678b738d223e636fd
stephanedecerf <decerfstephane1972@gmail.com>
2025-02-27 17:27:29 +0100

    modification du footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,23 +1,23 @@
 <footer class="text-center pt-4 pb-1 mt-auto shadow bg-light">
     <div class="footer">
-        <a href="#" class="text-decoration-none text-dark">Mentions légales</a>
-        <p class="pt-3 copyrights-text">--- Stan Anou - 2025 --- </p>
+
+        <div class="row">
+            <div class="col border">
+                <iframe class="border" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5180.908219159973!2d0.1694619!3d49.513701999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e02fe40985f1ff%3A0x89e1a03a20457b7b!2sAfpa%20-%20Centre%20du%20Havre!5e0!3m2!1sfr!2sfr!4v1740572627489!5m2!1sfr!2sfr" width="400" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col text-center border">
+           <h3>Nos valeurs</h3>
+            <p>Recyclae /     Dépollution /    Garantie 3 Mois</p>
+            <br><br><br><br>
+            <p>Mention légale</p>
+            
+            </div>
+        </div>
 
 
-        <!-- <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div> -->
+
+
+
 
 
 
